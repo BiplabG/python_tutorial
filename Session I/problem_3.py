71cd9209bbeb3cc7211f40ea6edbf2aedd034da3
Biplab Gautam <biplab.gautam.9@gmail.com>
2020-11-05 15:28:43 +0545

    Session I

---
+++ Session I/problem_3.py
@@ -0,0 +1,6 @@
+name = input("Name:\n")
+address = input("Address:\n")
+hobby = input("Hobby:\n")
+
+print("You are %s. You live in %s. You like to do %s in your spare time." %(name, address, hobby))
+print(f"You are {name}. You live in {address}. You like to do {hobby} in your spare time.")
---
+++ Session I/problem_4.py
@@ -0,0 +1,4 @@
+pi = 3.1415
+radius = float(input("Enter the radius: \n"))
+area = pi*(radius**2)
+print(f"Area of circle: {area}")
